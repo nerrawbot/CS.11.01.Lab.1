3680555c9a64a55944a6c09ce660c7d1cf8e6d33
chengqifengwarren <chengqifeng2009@gmail.com>
2024-08-23 12:32:58 +0800

    Huh

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input your age: ");
+        int age = input.nextInt();
+        int Newage = age/2 + 7;
+        System.out.println(age + " year olds should date someone who is " + Newage + " years old.");
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input your age: ");
+        int age = input.nextInt();
+        int Page = 100 - age;
+        System.out.println(age + " year olds should at least " + Page + " pages before giving up on a book.");
     }
 }
